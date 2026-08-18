0503317eff61abc738b93b2ccc3429ae4deafad2
Gabriela Edmundo <edmundogabriela@gmail.com>
2026-08-17 22:45:52 -0300

    feat(ap01-git): implementa opcao de multiplicacao no menu

--- menu.py
+++ menu.py
@@ -3,16 +3,22 @@ import calculadora
 def menu():
     while True:
         print("1. Somar")        
-        print("2. Subtrair")        
+        print("2. Subtrair")  
+        print("3. Multiplicar")  
 
-        opcao = int(input("Escolha uma opção: "))        
+        opcao = int(input("\nEscolha uma opção: "))        
 
         if opcao == 1:
-            a = float(input("Digite o primeiro número: "))
+            a = float(input("\nDigite o primeiro número: "))
             b = float(input("Digite o segundo número: "))
-            print (f'{a} + {b} = {calculadora.somar(a, b)}')
+            print (f'\n{a} + {b} = {calculadora.somar(a, b)}')
 
         if opcao == 2:
-            a = float(input("Digite o primeiro número: "))
+            a = float(input("\nDigite o primeiro número: "))
             b = float(input("Digite o segundo número: "))
-            print (f'{a} - {b} = {calculadora.subtrair(a, b)}')
+            print (f'\n{a} - {b} = {calculadora.subtrair(a, b)}')
+
+        if opcao == 3:
+            a = float(input("\nDigite o primeiro número: "))
+            b = float(input("Digite o segundo número: "))
+            print (f'\n{a} * {b} = {calculadora.multiplicar(a, b)}')
